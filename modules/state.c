9cb5525b9b9fef69110fd061d561370fb724a691
danaikar <danaikar02@gmail.com>
2021-03-26 20:43:43 +0200

    Aσκηση 3

--- modules/state.c
+++ modules/state.c
@@ -5,12 +5,17 @@
 #include "ADTList.h"
 #include "state.h"
 
-int* create_int(int value)
-	{
-		int* pointer = malloc(sizeof(int));		// δέσμευση μνήμης
-		*pointer = value;						// αντιγραφή του value στον νέο ακέραιο
-		return pointer;
-	}
+// int* create_int(int value){
+// 	int* pointer = malloc(sizeof(int));		// δέσμευση μνήμης
+// 	*pointer = value;						// αντιγραφή του value στον νέο ακέραιο
+// 	return pointer;
+// }
+int end_game = 0;
+
+int compare_entrances(Pointer a, Pointer b){
+	return *(int*)a - *(int*)b; 
+}
+
 // Οι ολοκληρωμένες πληροφορίες της κατάστασης του παιχνιδιού.
 // Ο τύπος State είναι pointer σε αυτό το struct, αλλά το ίδιο το struct
 // δεν είναι ορατό στον χρήστη.
@@ -87,13 +92,13 @@ State state_create() {
 			obj->type = PORTAL;						// είναι πύλη.
 			obj->rect.width = 100;
 			obj->rect.height = 5;
-
-		} else if(rand() % 2 == 0) {				// Για τα υπόλοιπα, με πιθανότητα 50%
+		}
+		else if(rand() % 2 == 0) {				// Για τα υπόλοιπα, με πιθανότητα 50%
 			obj->type = OBSTACLE;					// επιλέγουμε εμπόδιο.
 			obj->rect.width = 10;
 			obj->rect.height = 80;
-
-		} else {
+		} 
+		else {
 			obj->type = ENEMY;						// Και τα υπόλοιπα είναι εχθροί.
 			obj->rect.width = 30;
 			obj->rect.height = 30;
@@ -112,24 +117,24 @@ State state_create() {
 	List portal_pairs = list_create(NULL);
 
 	int t[PORTAL_NUM/2];                                   // πινακας με περιεχομενα τα exit που εχω πετυχει 
-	for(int i = 0; i < PORTAL_NUM/2; i++) {     // odd !?
+	for(int i = 0; i < PORTAL_NUM/2; i++) {       // odd !?
 		
 		PortalPair pair = malloc(sizeof(*pair));
 		//αρχικοποιηση των pairs
 		Object entrance = malloc(sizeof(*entrance));
 		Object exit = malloc(sizeof(*exit));
-		pair->entrance = i+1;
+		pair->entrance = i+1;                      // cast !
 
 		int k = rand() % 100 + 1;  
 		int j = 0;
 		while(j < 50)
-			if(k == t[j]) { k = rand() % 100 + 1;   j = 0;}	
+			if(k == t[j]) { k = rand() % 100 + 1;   j = 0; }	
 			else	j++;
 
-		pair->exit = k;
+		pair->exit = k;								// cast !
 		t[i] = k;
 
-		printf("%d, %d\n",pair->entrance,pair->exit);
+		//printf("%d, %d\n",pair->entrance,pair->exit);
 	 	list_insert_next(portal_pairs, list_last(portal_pairs), pair); 
 	}
 
@@ -139,18 +144,7 @@ State state_create() {
 // Επιστρέφει τις βασικές πληροφορίες του παιχνιδιού στην κατάσταση state
 
 StateInfo state_info(State state) {				// Προς υλοποίηση
-	
-	// StateInfo state_new = malloc(sizeof(*state_new));    // free (???)
-
-	// struct state_new {
-	// 	Object character;				
-	// 	int current_portal;				
-	// 	int wins;						
-	// 	bool playing;				
-	// 	bool paused;
-	// };
-
-	return &(state->info) ; 
+	return &(state->info); 
 }
 
 // Επιστρέφει μια λίστα με όλα τα αντικείμενα του παιχνιδιού στην κατάσταση state,
@@ -174,11 +168,87 @@ List state_objects(State state, float x_from, float x_to) {			// Προς υλο
 	return list;
 }
 
+
 // Ενημερώνει την κατάσταση state του παιχνιδιού μετά την πάροδο 1 frame.
 // Το keys περιέχει τα πλήκτρα τα οποία ήταν πατημένα κατά το frame αυτό.
 
-void state_update(State state, KeyState keys) {
-	// Προς υλοποίηση
+void state_update(State state, KeyState keys) {		
+	// character update
+	if(keys->enter == false && keys->left == false && keys->right == false && keys->up == false && keys->n == false && keys->p == false )
+		state->info.character->rect.x +=7;
+	else if(keys->right != false)
+		state->info.character->rect.x +=12;
+	else if(keys->left != false){
+		state->info.character->forward = false;
+		//state->info.character->rect.x -=7;
+	}
+	else if(keys->up != false){
+		if(state->info.character->rect.y < 220)
+			state->info.character->rect.y -= 15;
+		else	
+			state->info.character->rect.y += 15; 
+	}
+	else if(keys->enter != false && end_game == 1){
+		end_game = 0;
+		state_create();
+	}
+	else if(keys->p != false){
+		if(state->info.playing == 1)	state->info.playing == 0;
+		else 	state->info.playing == 1;
+		//if(keys->n != false)
+	}
+		
+
+	// enemies update
+	for(int i = 0; i < 4*PORTAL_NUM; i++){
+		Object obj = vector_get_at(state->objects, i);
+		if(obj->type == ENEMY)	{	
+			obj->rect.x += 5;
+			if(state->info.character->rect.x == obj->rect.x) { // συγκρουση εχθρου με εμποδιο 
+				end_game = 1; // Μηνυμα για enter !!
+				exit(1);
+			}	                                     
+			else {
+				for(int j = 0; j < 4*PORTAL_NUM; j++){
+					Object obj2 = vector_get_at(state->objects, i);
+					if(obj2->type == OBSTACLE && obj->rect.x == obj2->rect.x)
+						obj->forward = true;
+				}
+			}	
+		}
+		else if(obj->type == OBSTACLE) {
+			if(state->info.character->rect.x == obj->rect.x){  // συγκρουση χαρακτηρα με εμποδιο
+				end_game = 1;
+				exit(1);
+			}									  // Μηνυμα για enter !!
+		}
+		else {                                                // συγκρουση εμποδιου ή εχθρου με πυλη 
+			int count = 0;       							  // συνολο νικων                                          
+			Object obj = vector_get_at(state->objects, i);    // portal
+			for(int j = 0; j < 4*PORTAL_NUM; j++) {
+				Object obj2 = vector_get_at(state->objects, j);									
+				if((obj2->type != PORTAL) && (obj2->rect.x == obj->rect.x)) {
+					if(obj->forward == true) {
+						ListNode node = list_first(state->portal_pairs);
+						for(int k = 0; k < PORTAL_NUM; k++) {
+							PortalPair pair = malloc(sizeof(*pair)); 
+							pair = list_find(state->portal_pairs, node, compare_entrances);
+							if(pair->entrance == (j % 4) + 1) {
+								obj->rect.x = pair->exit->rect.x;
+								if((obj->type == CHARACTER) && (pair->exit == PORTAL_NUM)) {
+									count++;
+									state->info.character->rect.x = 0;				
+								}
+							}
+							node = list_next(state->portal_pairs, node);
+						}
+					}
+					
+				}
+			}
+		}
+	}
+
 }
 
 // Καταστρέφει την κατάσταση state ελευθερώνοντας τη δεσμευμένη μνήμη.
@@ -186,3 +256,6 @@ void state_update(State state, KeyState keys) {
 void state_destroy(State state) {
 	free(state);
 }
+
+
+
